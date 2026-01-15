76014cde039e478a25852738f2e44027486a7432
vinckobb <vinckobb@gmail.com>
2026-01-15 19:02:22 +0100

    Upgraded angular to 21, added keycloak-js

--- app/pages/+default/home/home.component.ts
+++ app/pages/+default/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
 import {ComponentRedirectRoute} from '@anglr/common/router';
-import {Authorize, ComponentRouteAuthorized} from '@anglr/authentication';
+import {ComponentRouteAuthorized} from '@anglr/authentication';
 import {DebugDataCopyClickModule} from '@anglr/common/material';
 import {WithScrollableCssClass} from '@anglr/common';
 
@@ -26,7 +26,6 @@ import {AnimateRouteDirective} from '../../../directives';
 })
 @ComponentRedirectRoute('', 'home')
 @ComponentRouteAuthorized({path: 'home'})
-@Authorize('home-page')
 @WithScrollableCssClass()
 export class HomeComponent
 {
